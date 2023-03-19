e51628127ee88940e21085d93e584ff039044900
Yonatan Gideoni <yonatan.gideoni@gmail.com>
2023-03-19 11:01:16 +0000

    Test finally working

--- tests/test_gnn.py
+++ tests/test_gnn.py
@@ -11,29 +11,30 @@ from oil.datasetup.datasets import split_dataset
 from oil.utils.utils import LoaderTo
 
 
-def trans_x_vals(coords):
-    trans_coords = coords.clone()
-    trans_coords[:, :, 0] += 1
-    return (coords ** 2).sum(axis=-1).unsqueeze(-1), (trans_coords ** 2).sum(axis=-1).unsqueeze(-1), trans_coords
-
-
-def test_gnn_conv_equivariance(groups_and_coords2vals: tuple = ((T(2), trans_x_vals), (Trivial(), trans_x_vals)),
-                               bs: int = 1, device='cpu', h: int = 10, w: int = 10, liftsamples: int = 1,
-                               n_mc_samples: int = 25, ds_frac: float = 1., fill: float = 1., nbhd_size: int = -1,
-                               conv_layer=LieGNNSimpleConv):
+def test_gnn_equiv():
+    groups: tuple = (T(2), SO2(.05))
+    bs: int = 1
+    device = 'cpu'
+    h: int = 1
+    w: int = 1
+    liftsamples: int = 1
+    n_mc_samples: int = 25
+    ds_frac: float = 1.
+    fill: float = 1.
+    nbhd_size: int = -1
+    conv_layer = LieGNNSimpleConv
     device = torch.device(device)
 
     # Construct coordinate grid
     i = torch.linspace(-h / 2., h / 2., h + 1)
     j = torch.linspace(-w / 2., w / 2., w + 1)
     orig_coords = torch.stack(torch.meshgrid([i, j]), dim=-1).float().view(-1, 2).unsqueeze(0).repeat(bs, 1, 1)
-    for group, vals_from_coords in groups_and_coords2vals:
-        coords = orig_coords.clone()
-        values = torch.randn((bs, coords.shape[1], 1))
+    for group in groups:
+        coords = orig_coords.clone() + 12
 
-        mask = torch.ones(bs, values.shape[1], device=device) > 0
-
-        vals1, vals2, trans_coords = vals_from_coords(coords)
+        vals1 = torch.tensor(np.array([[1], [1], [0], [0]])).unsqueeze(0)
+        vals2 = torch.tensor(np.array([[0], [0], [1], [1]])).unsqueeze(0)
+        mask = torch.ones(bs, vals1.shape[1], device=device) > 0
         graph1 = makeGraph((coords, vals1, mask), 1, group, nbhd_size=nbhd_size, liftsamples=liftsamples)
         graph2 = makeGraph((coords, vals2, mask), 1, group, nbhd_size=nbhd_size, liftsamples=liftsamples)
 
@@ -48,15 +49,8 @@ def test_gnn_conv_equivariance(groups_and_coords2vals: tuple = ((T(2), trans_x_v
                                 edge_index=graph2.edge_index,
                                 edge_attr=graph2.edge_attr)
 
-        coord_val_map1 = {tuple(coord): float(val) for coord, val in
-                          zip(coords.flatten(end_dim=-2).numpy(), res1.flatten())}
-        coord_val_map2 = {tuple(coord): float(val) for coord, val in
-                          zip(trans_coords.flatten(end_dim=-2).numpy(), res2.flatten())}
-        print(abs(np.array([coord_val_map1[c1] - coord_val_map2[c2]
-                            for c1, c2 in zip(coord_val_map1, coord_val_map2)])).mean(), group)
-        print()
-        # assert torch.isclose(res1, res2).all(), f'Error - layer is not equivariant! Group - {group}'
-
-
-if __name__ == "__main__":
-    test_gnn_conv_equivariance()
+        if isinstance(group, T):
+            assert np.isclose(res1[:2, 0].detach(), res2[2:, 0].detach()).all(), 'Error: T2 is not equivariant!'
+        else:  # SO2
+            assert not np.isclose(res1[:2, 0].detach(), res2[2:, 0].detach()).all(), \
+                'Error - why the hell is SO2 equivariant here? Sometimes happens, super weird'
