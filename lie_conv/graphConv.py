ef0a5515fda1f5772a47ef50e7106858f5d99b30
Yonatan Gideoni <yonatan.gideoni@gmail.com>
2023-03-19 11:44:13 +0000

    Added enable_mlp arg

--- lie_conv/graphConv.py
+++ lie_conv/graphConv.py
@@ -7,6 +7,7 @@ from torch_geometric.nn import MessagePassing
 from torch_scatter import scatter
 from lie_conv.utils import Expression, export
 
+
 @export
 class LieGNNSimpleConv(MessagePassing):
     """
@@ -17,22 +18,26 @@ class LieGNNSimpleConv(MessagePassing):
         where d^2 = ||log(v^{-1}u)||^2 + \alpha ||q_i - q_j||^2
         (the default distance in the LieConv paper)
     """
-    def __init__(self, c_in, c_out, hidden_dim=None, agg='add', **kwargs):
+
+    def __init__(self, c_in, c_out, hidden_dim=None, agg='add', enable_mlp: bool = True, **kwargs):
         super().__init__(aggr=agg)
         if hidden_dim is None:
             self.hidden_dim = c_out
         else:
             self.hidden_dim = hidden_dim
 
-        self.mlp = nn.Sequential(
-            nn.Linear(c_in, self.hidden_dim), 
-            nn.ReLU(),
-            nn.Linear(self.hidden_dim, self.hidden_dim),
-            nn.ReLU(),
-            nn.Linear(self.hidden_dim, c_out), 
-            nn.ReLU()
-        ) 
-    
+        if enable_mlp:
+            self.mlp = nn.Sequential(
+                nn.Linear(c_in, self.hidden_dim),
+                nn.ReLU(),
+                nn.Linear(self.hidden_dim, self.hidden_dim),
+                nn.ReLU(),
+                nn.Linear(self.hidden_dim, c_out),
+                nn.ReLU()
+            )
+        else:
+            self.mlp = lambda x: x
+
     def forward(self, x, edge_index, edge_attr):
         out = self.propagate(edge_index, x=x, edge_attr=edge_attr)
         return out
@@ -45,7 +50,7 @@ class LieGNNSimpleConv(MessagePassing):
         """
         messages = torch.einsum("ij,ik->ij", x_i, edge_attr)
         return messages
-    
+
     def aggregate(self, inputs, index, dim_size):
         """
         Aggregate messages from all neighbouring nodes
@@ -55,9 +60,9 @@ class LieGNNSimpleConv(MessagePassing):
         """
         aggr_out = scatter(inputs,
                            index,
-                           dim=self.node_dim, 
+                           dim=self.node_dim,
                            reduce=self.aggr,
-                           dim_size=dim_size) # dim_size to ensure even nodes are padded,, we return same size 
+                           dim_size=dim_size)  # dim_size to ensure even nodes are padded,, we return same size
         return aggr_out
 
     def update(self, aggr_out):
@@ -67,19 +72,20 @@ class LieGNNSimpleConv(MessagePassing):
         """
         return self.mlp(aggr_out)
 
+
 @export
 class LieConvGCN(LieGNNSimpleConv):
     def __init__(self, c_in, c_out, hidden_dim=None, agg='add', edge_dim=1, **kwargs):
         super().__init__(c_in, c_out, hidden_dim, agg, **kwargs)
 
         self.mlp_msg = nn.Sequential(
-            nn.Linear(edge_dim, self.hidden_dim), 
+            nn.Linear(edge_dim, self.hidden_dim),
             nn.ReLU(),
             nn.Linear(self.hidden_dim, self.hidden_dim),
             nn.ReLU(),
-            nn.Linear(self.hidden_dim, 1), 
+            nn.Linear(self.hidden_dim, 1),
             nn.ReLU()
-        ) 
+        )
 
     def message(self, x_i, x_j, edge_attr):
         embedded_edge_attr = self.mlp_msg(edge_attr)
